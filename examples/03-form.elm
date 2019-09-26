52a468e985f1fbb7354be7b75b3f29b5f17cc1c2
kento tajiri <k.denjiry@gmail.com>
2019-09-26 16:19:11 +0900

    Add validation for too short pass

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -73,6 +73,9 @@ viewInput t p v toMsg =
 viewValidation : Model -> Html msg
 viewValidation model =
   if model.password == model.passwordAgain then
-    div [ style "color" "green" ] [ text "OK" ]
+    if String.length(model.password) >= 8 then
+      div [ style "color" "green" ] [ text "OK" ]
+    else
+      div [ style "color" "red" ] [ text "too short" ]
   else
     div [ style "color" "red" ] [ text "Passwords do not match!" ]
